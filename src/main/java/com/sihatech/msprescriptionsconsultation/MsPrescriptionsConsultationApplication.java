61ef82dc86c1ab1bc996ba0edb57222d7f771090
sabrinaDz <sabrinasahli@live.fr>
2021-09-27 01:59:59 -0700

    init project #1

---
+++ src/main/java/com/sihatech/msprescriptionsconsultation/MsPrescriptionsConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.sihatech.msprescriptionsconsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPrescriptionsConsultationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPrescriptionsConsultationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sihatech/msprescriptionsconsultation/MsPrescriptionsConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihatech.msprescriptionsconsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPrescriptionsConsultationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
